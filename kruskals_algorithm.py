7209bd8120fb7823e8c522118d715a794a9ed502
dkalchenko01 <dkalchenko@kse.org.ua>
2025-04-09 14:51:05 +0300

    finished version of kruskals algorithm

--- kruskals_algorithm.py
+++ kruskals_algorithm.py
@@ -23,24 +23,28 @@ def kruskals_algorithm(sorted_list, n):
     minimum_spanning_tree = []
     is_added = set()
     for edge in sorted_list:
-        if ((edge[0] not in is_added) or (edge[1] not in is_added)) and (not check_cycle(edge[0], edge[1], minimum_spanning_tree)):
+        if ((edge[0] not in is_added) or (edge[1] not in is_added)) or (not check_cycle(edge[0], edge[1], minimum_spanning_tree)):
             minimum_spanning_tree.append(edge)
             is_added.add(edge[0])
             is_added.add(edge[1])
-        if len(minimum_spanning_tree) > n - 1:
-            print("Sorry, graph is not connected. minimum spanning tree doesn't exist")
-            break
+    if len(minimum_spanning_tree) != n - 1:
+        return []
     return minimum_spanning_tree
 
 def check_cycle(v1, v2, tree):
-    v1_neighbours = [x[1] for x in tree if x[0] == v1]
-    v1_neighbours += [x[0] for x in tree if x[1] == v1]
-    if v2 in v1_neighbours:
+    visited = []
+    if v2 in dfs(v1, tree, visited):
         return True
-    elif len(v1_neighbours) <= 1:
-        return False
-    else:
-        for neighbour in v1_neighbours:
-            check_cycle(neighbour, v2, tree)
+    return False
+
+def dfs(v, tree, visited_list : list):
+    visited_list.append(v)
+    neighbours = [x[1] for x in tree if x[0] == v]
+    neighbours += [x[0] for x in tree if x[1] == v]
+    for neighbour in neighbours:
+        if neighbour not in visited_list:
+            dfs(neighbour, tree, visited_list)
+    return visited_list
+
 
 
